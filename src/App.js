e4123c4a014be5b69ab07e88b35fa7045ba79ea0
Rovine1999 <rovinewanjala99@gmail.com>
2023-01-03 17:32:45 +0300

    search api for countries and cities

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import Search from './components/search/search';
 import './App.css';
 
 function App() {
+
+  
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  }
+
   return (
     <div className="container">
-     <Search />
+     <Search OnSearchChange={handleOnSearchChange} />
     </div>
   );
 }
---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f24228071cmsh7b9adf3a79203efp1bfb77jsnb8696b16dbfa',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
